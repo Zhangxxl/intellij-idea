1997fbadda370075ac1cd6912716bc965bf3c9a1
zhang-xx <qq957413212@hotmail.com>
2019-05-10 13:42:15 +0800

    IU-2019.1.2 <Administrator@YC-20180627EGIW Create project.default.xml, Enum.java, Interface.java, Default.xml, applicationLibraries.xml, file.template.settings.xml, ignore.xml, find.xml, SQL Script.sql, File Header.java, keymap.xml, _@user_Darcula.icls, Default.xml, scala.xml, editor.codeinsight.xml, editor.xml, debugger.xml, updates.xml, baseRefactoring.xml, ide.general.xml, git.xml, _@user_Default.icls, ide-features-trainer.xml, CodeGlance.xml, MyDefault.icls, rainbow_brackets.xml, Singleton.java, pgyer_Dialog_Plugin_persist.xml, gradle.experimental.xml, terminal.xml, androidStudioFirstRun.xml, ui.lnf.xml, filetypes.xml, plugin_ui.xml, colors.scheme.xml, notifications.xml, databaseDrivers.xml, AnnotationType.java, customization.xml, studioFlags.xml, laf.xml, parameter.hints.xml, Class.java, jdk.table.xml, path.macros.xml, web-browsers.xml, _@user_Atom One Dark.icls, proxy.settings.xml, vcs.xml, _@user_Material One Dark.icls, databaseSettings.xml, toStringTemplates.xml, usageView.xml

---
+++ fileTemplates/includes/File Header.java
@@ -0,0 +1,12 @@
+/**
+ * Copyright Zhangxx. 
+ * Copyright (c) ${YEAR}
+ * All rights reserved
+ * 
+ * @project  : ${PROJECT_NAME}
+ * @package  : ${PACKAGE_NAME}
+ * @author   : zhangxx
+ * @date     : ${DATE}
+ * @email    : qq957413212@hotmail.com
+ * @desc     : //TODO 
+ */
